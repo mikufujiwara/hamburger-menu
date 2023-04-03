2004e99e35a51926cc2759b80cc15a13111e2a17
mikufujiwara <llt9.oct22@gmail.com>
2023-04-02 21:30:26 -0700

    modal added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const hamburger = document.querySelector(".hamburger");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const closeIcon = document.querySelector(".close-icon");
 const items = document.querySelector(".hamburger-items");
+const btn = document.getElementById("myBtn");
+const modal = document.getElementById("myModal");
+var closeBtn = document.getElementsByClassName("close")[0];
 
 window.addEventListener("DOMContentLoaded", () => {
   items.classList.toggle("hide");
@@ -13,3 +16,18 @@ hamburger.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("hide");
   closeIcon.classList.toggle("hide");
 });
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+    console.log(e.target);
+  }
+}
